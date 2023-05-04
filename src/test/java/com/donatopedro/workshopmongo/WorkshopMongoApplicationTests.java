70061fb23fd8363cc564a74a4c54e40f5a1e72b6
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-05-04 13:19:08 -0300

    Initial commit

---
+++ src/test/java/com/donatopedro/workshopmongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatopedro.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
