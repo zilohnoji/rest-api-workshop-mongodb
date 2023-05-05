517e6b74907efcf3b2b2666361937fa14d5fa26d
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-05-05 13:40:21 -0300

    Refactor and insert comments in posts

---
+++ src/main/java/com/donatopedro/workshopmongo/dto/CommentDTO.java
@@ -0,0 +1,47 @@
+package com.donatopedro.workshopmongo.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CommentDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Date date;
+	private AuthorDTO author;
+	private String text;
+
+	public CommentDTO() {
+	}
+
+	public CommentDTO(Date date, AuthorDTO author, String text) {
+		this.date = date;
+		this.author = author;
+		this.text = text;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
